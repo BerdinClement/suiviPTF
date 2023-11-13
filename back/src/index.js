7f0d83eebbc461dea44b1521b46701af4bebb90b
Samuel Caron <samuelcarondu9@gmail.com>
2023-11-13 12:20:09 +0100

    Swagger API

--- back/src/index.js
+++ back/src/index.js
@@ -8,12 +8,15 @@ const cookieParser = require('cookie-parser')
 const PORT = process.env.PORT || 9000;
 const Logger = require('./utils/logger');
 const logger = Logger.getInstance()
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger/swagger.json');
 
 require('./db')();
 
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const routes = glob.sync('./src/**/*.router.js');
 
