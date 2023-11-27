b85301e3707783f70df160ba79225945c4413351
marinaflament <marina.flament@axleration.fr>
2023-11-27 11:20:40 +0100

    Update InputLogin.tsx

--- front/components/InputLogin.tsx
+++ front/components/InputLogin.tsx
@@ -1,29 +1,37 @@
-"use client"
-
+"use client";
 
 interface InputProps {
-    type: string;
-    placeholder: string;
-    className: string;
-    inputValue: any;
-    setInputValue: (newInputValue:any)=>void;
+  type: string;
+  placeholder: string;
+  className: string;
+  inputValue: any;
+  setInputValue: (newInputValue: any) => void;
 }
 
-const InputLogin = ({ placeholder, type, className, inputValue, setInputValue }: InputProps) => {
-
-    return (
-        <div className="flex items-center justify-center mt-6 w-full">
-        <div className={`
+const InputLogin = ({
+  placeholder,
+  type,
+  className,
+  inputValue,
+  setInputValue,
+}: InputProps) => {
+  return (
+    <div className="flex items-center justify-center mt-6 w-full">
+      <div
+        className={`
                 relative
                 ${className}
-            `}>
-            <input
-            id={`${placeholder}`}
-            name={`${placeholder}`}
-            value={inputValue}
-            onChange={(e) => {setInputValue(e.target.value)}}
-            type={type}
-            className={`
+            `}
+      >
+        <input
+          id={`${placeholder}`}
+          name={`${placeholder}`}
+          value={inputValue}
+          onChange={(e) => {
+            setInputValue(e.target.value);
+          }}
+          type={type}
+          className={`
                 border-b 
                 border-gray-300 
                 py-1 
@@ -35,13 +43,22 @@ const InputLogin = ({ placeholder, type, className, inputValue, setInputValue }:
                 bg-inherit
                 w-full
                 `}
-            />
-            <label htmlFor={`${placeholder}`}
-            className={`absolute left-0 top-1 cursor-text ${inputValue == '' ? "peer-focus:text-xs" : "text-xs"} ${inputValue == '' ? "peer-focus:-top-4" : "-top-4"} ${inputValue == '' ? "peer-focus:text-purple" : "text-purple"} transition-all `}
-            >{placeholder}</label>
-        </div>
-        </div>
-    );
+        />
+        <label
+          htmlFor={`${placeholder}`}
+          className={`absolute left-0 top-1 cursor-text 
+            ${inputValue == "" ? "peer-focus:text-xs" : "text-xs"}
+            ${inputValue == "" ? "peer-focus:-top-4" : "-top-4"} 
+            ${inputValue == "" ? "peer-focus:text-purple" : "text-purple"}
+            ${inputValue != "" ? "peer-focus:text-xs" : "text-xs"}
+            ${inputValue != "" ? "peer-focus:-top-4" : "-top-4"}
+             transition-all `}
+        >
+          {inputValue == "" ? placeholder : ""}
+        </label>
+      </div>
+    </div>
+  );
 };
 
 export default InputLogin;
