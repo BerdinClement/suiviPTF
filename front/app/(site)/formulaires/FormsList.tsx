20e5573af6155622a98a3f501f530364fb5fca19
marinaflament <marina.flament@axleration.fr>
2023-11-28 09:52:01 +0100

    Form Filter

--- front/app/(site)/formulaires/FormsList.tsx
+++ front/app/(site)/formulaires/FormsList.tsx
@@ -1,39 +1,54 @@
-'use client'
+"use client";
 
 import FormCard from "@/components/FormCard";
 import { getAllForms } from "@/services/forms";
 import { useEffect, useState } from "react";
 
 interface FormProp {
-    title: string;
-    date: string;
-    _id: string;
+  title: string;
+  date: string;
+  _id: string;
 }
 
-const FormsList = () => {
-    const [forms, setForms] = useState([]);
+interface FormListProps {
+  activeTitle: string;
+  setActiveTitle: (title: string) => void;
+}
 
-    useEffect(() => {
-        const fetchStudents = async () => {
-            const res = await getAllForms();
-            setForms(res);
-        };
-        fetchStudents();
-    }, []);
+const FormsList = ({ activeTitle, setActiveTitle }: FormListProps) => {
+  const [forms, setForms] = useState([]);
 
-    if (!forms) {
-        return <div>Chargement...</div>;
-    }
+  useEffect(() => {
+    const fetchStudents = async () => {
+      const res = await getAllForms();
+      setForms(res);
+    };
+    fetchStudents();
+  }, []);
 
-    return (
-        <>
-                {
-                    forms.map((form: FormProp) => (
-                        <FormCard title={form.title} date={form.date} id={form._id} key={form._id} className="w-full md:w-80"/>
-                    ))
-                }
-        </>
-    );
+  if (!forms) {
+    return <div>Chargement...</div>;
+  }
+
+  return (
+    <>
+      {forms.map((form: FormProp, index) => {
+        activeTitle = activeTitle.toLowerCase();
+        const title = form.title.toLowerCase();
+        if (activeTitle === "" || title.includes(activeTitle)) {
+          return (
+            <FormCard
+              title={`${form.title}`}
+              date={form.date}
+              id={form._id}
+              key={form._id}
+              className="w-full md:w-80"
+            />
+          );
+        }
+      })}
+    </>
+  );
 };
 
 export default FormsList;
